4bf01975c30f9173893999d4deb3f2a922fecc4f
roart <roart@free>
2020-07-30 13:14:56 +0200

    Move to parameters.

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -2,32 +2,16 @@ import React, {Component} from 'react';
 import { Line } from 'react-chartjs-2';
 import { defaults } from 'react-chartjs-2'
 
-const data = (canvas) => {
-    defaults.global.defaultFontFamily = 'Montserrat';
-    defaults.global.defaultFontStyle = 'bold';
-    const context = canvas.getContext("2d");
-    var gradient = context.createLinearGradient(0, 0, 0, 250);
-    gradient.addColorStop(0, 'rgba(27,191,228,1)');
-    gradient.addColorStop(1, 'rgba(27,191,228,0)');   
-
-    return {
-  labels: ["Jun '19", "Jul '19", "Aug '19", "Sep '19", "Oct '19", "Nov '19", "Dec '19"],
-  datasets: [
-    {
-      label: 'Individuals',
-      fill: true,
-      backgroundColor: gradient,
-      lineTension: 0.5,
-      pointRadius: 0,
-      pointHitRadius: 10,
-      data: [8, 1, 3, 0, 0.5, 0, 10],
-      stepSize: 3
-    }
-  ]
-  }
-} ;
-
 export default class Chart extends Component {
+  constructor(props) {
+      super(props);
+      console.log(props.par);
+      console.log(props.data);
+      this.data2 = props.par;
+      this.data = props.data;
+   //this.colorPickerRef = React.createRef();
+  }
+
   render() {
     var chartOptions = {
       scales: {
@@ -52,7 +36,7 @@ export default class Chart extends Component {
 
     return (
       <div>
-        <Line ref="chart" data={data} options={chartOptions} />
+        <Line ref="chart" data={this.data} options={chartOptions} />
       </div>
     );
   }
--- src/components/MainBoard/MainBoard.jsx
+++ src/components/MainBoard/MainBoard.jsx
@@ -12,6 +12,7 @@ import SyncIcon from "../../assets/icons/SyncIcon.svg";
 import history from "../../assets/icons/history.svg";
 import verificationIcon from "../../assets/icons/verificationIcon.svg";
 import lockedPadlock from "../../assets/icons/lockedPadlock.svg";
+import { defaults } from 'react-chartjs-2'
 
 import styles from "./MainBoard.module.css";
 
@@ -61,6 +62,63 @@ const MainBoard = ({
     { id: 2, name: "Companies" },
   ];
 
+const data = (canvas) => {
+    defaults.global.defaultFontFamily = 'Montserrat';
+    defaults.global.defaultFontStyle = 'bold';
+    const context = canvas.getContext("2d");
+    var gradient = context.createLinearGradient(0, 0, 0, 250);
+    gradient.addColorStop(0, 'rgba(27,191,228,1)');
+    gradient.addColorStop(1, 'rgba(27,191,228,0)');   
+
+    return {
+  labels: ["Jun '19", "Jul '19", "Aug '19", "Sep '19", "Oct '19", "Nov '19", "Dec '19"],
+  datasets: [
+    {
+      label: 'Individuals',
+      fill: true,
+      backgroundColor: gradient,
+      lineTension: 0.5,
+      pointRadius: 0,
+      pointHitRadius: 10,
+      data: [8, 1, 3, 0, 0.5, 0, 10],
+      stepSize: 3
+    }
+  ]
+  }
+} ;
+
+const data2 = [
+  {
+      name: "Jun '19", uv: 5
+  },
+  {
+      name: "Jul '19", uv: 1
+  },
+  {
+      name: "Aug '19", uv: 9
+  },
+  {
+      name: "Sep '19", uv: 6
+  },
+  {
+      name: "Oct '19", uv: 7
+  },
+  {
+      name: "Nov '19", uv: 0.1
+  },
+  {
+      name: "Dec '19", uv: 8
+  },
+];
+
+    const defs = () => {
+	return (
+	    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+          <stop offset="5%" stopColor="#f853e7" stopOpacity={0.8}/>
+          <stop offset="95%" stopColor="#f853e7" stopOpacity={0}/>
+	    </linearGradient>
+	); };
+    
   return (
     <div className={styles.wrapper}>
       <div className={styles.mainBoard}>
@@ -82,12 +140,12 @@ const MainBoard = ({
           <div className={styles.display}>
             <div className={styles.displayCharts}>
               <div className={styles.upperChart}>
-		<Chart/>
+		<Chart data={data} />
                 {/* Individuals Chart */}
                 {/* Required data: individuals */}
               </div>
               <div className={styles.lowerChart}>
-		<Chart2/>
+		<Chart2 data={data2} name="Companies" defs={defs()} />
                 {/* Companies Chart */}
                 {/* Required data: companies */}
               </div>
